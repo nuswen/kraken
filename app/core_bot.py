d121899d14be80e18a6eed448fa3e4c576f1ac8c
nuswen <donn.nus@gmail.com>
2019-05-22 15:34:02 +0300

    init

--- app/core_bot.py
+++ app/core_bot.py
@@ -9,6 +9,10 @@ from tele_bot_tools import *
 @bot.message_handler(commands=['start'])
 def hi_msg(message):
     poster(bot, message.chat.id, 'fff')
+
+@bot.message_handler(content_types=['text'])
+def any_messages(msg):
+    poster(bot, msg.chat.id, msg)
     
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
