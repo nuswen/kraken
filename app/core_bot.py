f947f236ae03b8ebf514a2398f274285b5cdd761
nuswen <donn.nus@gmail.com>
2019-05-22 15:23:09 +0300

    init

--- app/core_bot.py
+++ app/core_bot.py
@@ -2,12 +2,13 @@ import tele_bot_tools as tbt
 from os import environ
 from app import bot
 import random
+from tele_bot_tools import *
                 
 
 
 @bot.message_handler(commands=['start'])
 def hi_msg(message):
-    pass
+    poster(bot, msg.chat.id, 'fff')
     
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
--- tele_bot_tools.py
+++ tele_bot_tools.py
@@ -1,8 +1,26 @@
 from telebot import types
 
+def poster(bot, chat_id, text, buttons=None, ed=False, message_id=None):
+    if buttons:
+        if ed:
+            bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text, reply_markup=keyboarder(buttons))
+        else:
+            bot.send_message(chat_id, text, reply_markup=keyboarder(buttons))
+    else:
+        if ed:
+            bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text)
+        else:
+            bot.send_message(chat_id, text)
 
-def order_row_keyboard(keys):
-    markup = types.ReplyKeyboardMarkup()
+
+def keyboarder(keys):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     for key in keys:
-        markup.row(key)
-    return markup
+        keyboard.add(types.KeyboardButton(text=key))
+    return keyboard
+
+
+def get_phone_number(bot, chat_id, text):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
+    keyboard.add(button_phone)
