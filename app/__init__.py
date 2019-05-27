f5080bd41d7c80994f867d78bd57e566da9a17b8
nuswen <donn.nus@gmail.com>
2019-05-27 14:44:10 +0300

    init

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     ur = models.messages.query.filter_by(id=1).first()
     am = models.messages.query.filter_by(id=2).first()
-    re = ur+';'+str(am)
+    re = ur+';'
     return (re), 200
 
 
