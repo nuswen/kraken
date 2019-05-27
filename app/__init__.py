cd7cbbf876618e5ddce397e0ca320c3881b93eb7
nuswen <donn.nus@gmail.com>
2019-05-27 19:09:41 +0300

    init

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def getMessage():
 def zen():
     ur = models.messages.query.filter_by(id=1).first()
     am = models.messages.query.filter_by(id=2).first()
-    re = am
+    re = '!'
     return (re), 200
 
 bot.remove_webhook()
