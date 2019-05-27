68bd56852e8d41e784af356ee0868bb7935feb77
nuswen <donn.nus@gmail.com>
2019-05-27 18:48:52 +0300

    init

--- app/__init__.py
+++ app/__init__.py
@@ -16,12 +16,12 @@ def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return ("!"), 200
 
-@app.route("/zenno/", methods=['GET'])
+@app.route("/zenno/", methods=['POST'])
 def zen():
-    ur = models.messages.query.filter_by(id=1).first()
-    am = models.messages.query.filter_by(id=2).first()
-    re = ur+';'+am
-    return (re), 200
+    #ur = models.messages.query.filter_by(id=1).first()
+    #am = models.messages.query.filter_by(id=2).first()
+    #re = ur+';'+am
+    return ("!"), 200
 
 bot.remove_webhook()
 bot.set_webhook(url=environ['app_url']+environ['token'])
