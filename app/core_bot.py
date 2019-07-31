39cd58dfe73d642bf8c084f8aaac16e6f2bb464d
nuswen <donn.nus@gmail.com>
2019-07-31 06:56:42 +0300

    init

--- app/core_bot.py
+++ app/core_bot.py
@@ -33,7 +33,7 @@ def any_messages(msg):
 
             poster(bot, msg.chat.id, 'Ща')
         except Exception as e:
-            poster(bot, msg.chat.id, e) #'Сначала ссылку')
+            poster(bot, msg.chat.id, e + '\n' + url + '\n' + amount) #'Сначала ссылку')
     except:
         if message.find('ribbble.com') > 0:
             models.messages.query.filter_by(id=1).delete()
