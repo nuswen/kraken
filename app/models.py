1aec62d428c19387c84063c3d139427979284fd8
nuswen <donn.nus@gmail.com>
2019-07-31 15:23:22 +0300

    init

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ class messages(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text)
 
-class Product(db.Model):
-    Id = db.Column(db.Integer, primary_key=True)
-    Url = db.Column(db.Text)
-    Amount = db.Column(db.Integer)
+class product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text)
+    amount = db.Column(db.Integer)
