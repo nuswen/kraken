fb23c4136b84a5334e2a71703874d97683be5b32
nuswen <donn.nus@gmail.com>
2019-10-14 19:45:43 +0300

    init

--- app/core_bot.py
+++ app/core_bot.py
@@ -14,7 +14,7 @@ def hi_msg(message):
 
 @bot.message_handler(commands=['time'])
 def time(message):
-    timeT = models.time.query.first()
+    timeT = models.time.first()
     poster(bot, message.chat.id, timeT.time)
 
 @bot.message_handler(content_types=['text'])
